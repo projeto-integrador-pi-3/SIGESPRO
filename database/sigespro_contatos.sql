64aff0f35117de3ca2b3fc4a8d77a88c2c65b785
amanda-holanda <amanda.holanda@jusbrasil.com.br>
2026-05-11 22:50:40 -0300

    refactor: remove modulos antigos e limpa componentes obsoletos

--- database/sigespro_contatos.sql
+++ database/sigespro_contatos.sql
@@ -1,46 +1,15 @@
--- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: sigespro
--- ------------------------------------------------------
--- Server version	8.0.46
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `contatos`
---
-
-DROP TABLE IF EXISTS `contatos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contatos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `endereco` text,
-  `telefone` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `responsavel` varchar(100) DEFAULT NULL,
-  `tipo` enum('subprefeitura','secretaria','fornecedor') NOT NULL,
+CREATE TABLE IF NOT EXISTS `contatos` (
+  `id`              int          NOT NULL AUTO_INCREMENT,
+  `nome`            varchar(100) NOT NULL,
+  `endereco`        text,
+  `telefone`        varchar(50)  DEFAULT NULL,
+  `email`           varchar(100) DEFAULT NULL,
+  `responsavel`     varchar(100) DEFAULT NULL,
+  `tipo`            enum('subprefeitura','secretaria','fornecedor') NOT NULL,
+  `area`            varchar(10)  DEFAULT NULL,
+  `numero_sei`      varchar(50)  DEFAULT NULL,
+  `numero_contrato` varchar(50)  DEFAULT NULL,
+  `vigencia_inicio` date         DEFAULT NULL,
+  `vigencia_fim`    date         DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-05-08 16:22:10
